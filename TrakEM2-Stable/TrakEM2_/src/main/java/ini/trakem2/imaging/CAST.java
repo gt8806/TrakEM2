7f5fee46ec9fb2e96b3fe4537ced4cf9f9e9524c
Gus Tropea <gus@Guss-MacBook-Air.local>
2017-05-31 17:59:26 -1000

    not sure

--- TrakEM2-Stable/TrakEM2_/src/main/java/ini/trakem2/imaging/CAST.java
+++ TrakEM2-Stable/TrakEM2_/src/main/java/ini/trakem2/imaging/CAST.java
@@ -11,6 +11,7 @@ import ij.process.ImageProcessor;
 import ij.ImageStack;
 import trainableSegmentation.WekaSegmentation;
 import weka.classifiers.functions.MultilayerPerceptron;
+import weka.core.Instances;
 import ini.trakem2.display.AreaContainer;
 import ini.trakem2.display.AreaWrapper;
 import ini.trakem2.display.AreaList;
@@ -42,22 +43,24 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.RejectedExecutionException;
 import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import levelsets.algorithm.Coordinate;
 
-public class AutomaticSegmentation {
+public class CAST {
 	
 	static public class Witchdoctor {
 		
 		private File model;
-		private String parentfolder = new File(AutomaticSegmentation.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getAbsoluteFile().getParent();
+		private String parentfolder = new File(CAST.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getAbsoluteFile().getParent();
 		private String traindata;
 		private String modelFile;
-		private final String[] labels = {"class 1","class 2"};
+		private String[] labels = new String[2];
 		private WekaSegmentation segment;
 		private MultilayerPerceptron nnet;
 		private ImagePlus classifiedImage;
 		
-	    Witchdoctor(ImagePlus imp, String fileName){
+		Witchdoctor(ImagePlus imp, String fileName){
 			// setting up the Witchdoctor.
 			traindata = parentfolder+File.separator+"trainingData"+File.separator+fileName+".arff";
 			modelFile = parentfolder+File.separator+"models"+File.separator+fileName+".model";
@@ -76,7 +79,27 @@ public class AutomaticSegmentation {
 		 */
 		public ImagePlus getClassification(){
 			segment.setClassifier(nnet);
-			segment.setClassLabels(labels);
+			try{
+				BufferedReader reader = new BufferedReader(new FileReader(traindata));
+				String line;
+				while((line = reader.readLine()) != null){
+					String[] splited = line.split(" ");
+					if(Arrays.asList(splited).contains("@attribute") && Arrays.asList(splited).contains("class")){
+						String classString = line.substring(line.indexOf('{')+1,line.indexOf('}'));
+						String newClassString = classString.replace("\'","");
+						String[] classes = newClassString.split(",");
+						labels[0] = classes[0];
+						labels[1] = classes[1];
+						break;
+					}
+				}
+				reader.close();
+				segment.setClassLabels(labels);
+			} catch (Exception e){
+				Utils.log2("class name: "+labels[0]);
+				Utils.log2("class name: "+labels[1]);
+			}
+			
 			//check to see if a model exists.
 			if(model.exists() && !model.isDirectory()){
 				segment.loadClassifier(modelFile);
@@ -125,26 +148,24 @@ public class AutomaticSegmentation {
 		// Permissible Deviation
 		public int boundaryDistance = 1;
 		// Name of the desired training file
-		public String fileName = "";
-		
+		public String fileName = getNames()[0];
 		//This function grabs the names of the files in the "trainingData" directory
-	    public String[] getNames(){
-	        String parentfolder = new File(AutomaticSegmentation.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getAbsoluteFile().getParent();
-	        String path = parentfolder+File.separator+"trainingData";
-	        File f = new File(path);
-	        File[] subFiles = f.listFiles();
-	        String[] fnames = new String[subFiles.length];
-	        for(int i = 0; i < subFiles.length; i++){
+		public String[] getNames(){
+			String parentfolder = new File(CAST.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getAbsoluteFile().getParent();
+			String path = parentfolder+File.separator+"trainingData";
+			File f = new File(path);
+			File[] subFiles = f.listFiles();
+			String[] fnames = new String[subFiles.length];
+			for(int i = 0; i < subFiles.length; i++){
 				if(subFiles[i].isFile()){
 					fnames[i] = subFiles[i].getName();
 				}
 			}
-	        return fnames;
+			return fnames;
 		}
 		
 		public String[] names = getNames();
-		
-	    /** In pixels, of the the underlying image to copy around the mouse. May be enlarged by shift+scrollwheel with PENCIL tool on a selected Displayable. */
+		/** In pixels, of the the underlying image to copy around the mouse. May be enlarged by shift+scrollwheel with PENCIL tool on a selected Displayable. */
 		public int width = 100, height = 100;
 		
 		public OptionPanel asOptionPanel() {
@@ -229,12 +250,12 @@ public class AutomaticSegmentation {
 		// Capture pointers before they are set to null
 		final AreaContainer ac = (AreaContainer)aw.getSource();
 		final AffineTransform source_aff = aw.getSource().getAffineTransform();
-		final Rectangle box = new Rectangle(x_p_w - AutomaticSegmentation.vp.width/2, y_p_w - AutomaticSegmentation.vp.height/2, AutomaticSegmentation.vp.width, AutomaticSegmentation.vp.height);
+		final Rectangle box = new Rectangle(x_p_w - CAST.vp.width/2, y_p_w - CAST.vp.height/2, CAST.vp.width, CAST.vp.height);
 		//sets up process for a new thread
 		Bureaucrat burro = Bureaucrat.create(new Worker.Task("Performing Voodoo") { public void exec() {
 			// Capture image as large as the vp width,height centered on x_p_w,y_p_w
-		    ImagePlus imp = (ImagePlus) layer.grab(box, 1.0, Patch.class, (int) aw.getSource().getAlpha(), Layer.IMAGEPLUS, ImagePlus.GRAY8);
-		    PointRoi roi = new PointRoi(box.width/2, box.height/2);
+			ImagePlus imp = (ImagePlus) layer.grab(box, 1.0, Patch.class, (int) aw.getSource().getAlpha(), Layer.IMAGEPLUS, ImagePlus.GRAY8);
+			PointRoi roi = new PointRoi(box.width/2, box.height/2);
 			imp.setRoi(roi);
 			Utils.log2("imp: " + imp);
 			Utils.log2("roi: " + imp.getRoi() + "    " + Utils.toString(new int[]{x_p_w - srcRect.x, y_p_w - srcRect.y}));
@@ -259,7 +280,7 @@ public class AutomaticSegmentation {
 					r.y = current.y;
 					area.add(new Area(r));
 					inside.add(current);
-				  
+					
 					final ComparablePoint neighbor1 = new ComparablePoint(current.x+1, current.y);
 					if(!inside.contains(neighbor1) && neighbor1.x < box.width - margin)
 						frontier.add(neighbor1);
@@ -284,10 +305,8 @@ public class AutomaticSegmentation {
 				IJError.print(nite);
 				return;
 			}
-			// This kind of works for 3D
-			AreaList al = (AreaList)(Display.getFront().getSelection().getSelected(AreaList.class)).get(0);
-			al.setArea(layer.getId(), area.createTransformedArea(aff));
-			al.calculateBoundingBox(layer);
+			aw.getArea().add(area.createTransformedArea(aff));
+			ac.calculateBoundingBox(layer);
 			Display.repaint(layer);
 		}}, layer.getProject());
 		//End of buro
